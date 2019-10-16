698925ad1b6047e4df9ea93afb246bff2007de9b
Faraaz Sareshwala <faraazs@gmail.com>
2019-10-16 15:09:11 -0700

    python: support formatting python code

--- format.sh
+++ format.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
+# format java
 gitroot=$(git rev-parse --show-toplevel)
-
 java_formatter=$gitroot/thirdparty/format/java/google-java-format-1.7-all-deps.jar
-find $gitroot -name '*.java' | xargs java -jar $java_formatter --replace
+find $gitroot/java -name '*.java' | xargs java -jar $java_formatter --replace
+
+# format python
+find $gitroot/python -name '*.py' | xargs bazel run thirdparty:format-python -- -pir --style=google

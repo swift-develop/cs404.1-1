80125e2b59413b4c896299edeb9aa6e37a6cecd1
Faraaz Sareshwala <faraazs@gmail.com>
2019-10-18 15:41:48 -0700

    update: remove unnecessary code

--- update.sh
+++ update.sh
@@ -2,19 +2,7 @@
 
 # This script merges in changes from the public assignment repository into private student forks.
 
-function log_error() {
-    echo "error: $1"
-    exit
-}
-
-function ensure_exists() {
-    program=$1
-    command -v "$program" > /dev/null 2>&1 || log_error "$program not available on path"
-}
-
 function main() {
-    ensure_exists git
-
     git remote remove upstream
     git remote add upstream git@github.com:fsareshwala/cs404.1.git
     git fetch upstream

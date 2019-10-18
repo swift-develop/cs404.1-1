45977c6ba580969bc1a86296982dadb9a03500e1
Faraaz Sareshwala <faraazs@gmail.com>
2019-10-17 18:10:24 -0700

    format: place in function main

--- format.sh
+++ format.sh
@@ -1,9 +1,14 @@
 #!/usr/bin/env bash
 
-# format java
-gitroot=$(git rev-parse --show-toplevel)
-java_formatter=$gitroot/thirdparty/format/java/google-java-format-1.7-all-deps.jar
-find $gitroot/java -name '*.java' | xargs java -jar $java_formatter --replace
+function main() {
+    # format java
+    gitroot=$(git rev-parse --show-toplevel)
+    java_formatter=$gitroot/thirdparty/format/java/google-java-format-1.7-all-deps.jar
+    find $gitroot/java -name '*.java' | xargs java -jar $java_formatter --replace
 
-# format python
-find $gitroot/python -name '*.py' | xargs bazel run thirdparty:format-python -- -pir --style=google
+    # format python
+    find $gitroot/python -name '*.py' | xargs \
+        bazel run thirdparty:format-python -- -pir --style=google
+}
+
+main "$@"

7068ca6c6b679968d053db10108cc14f221e76b1
Faraaz Sareshwala <faraazs@gmail.com>
2019-10-17 18:04:06 -0700

    nostdlib: add script to check students do not import from the standard library

---
+++ nostdlib.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+
+function join() {
+    local IFS="$1"
+    shift
+    echo "$*"
+}
+
+function main() {
+    java_blessed=(
+        "java.util.Arrays"
+        "java.util.Random"
+    )
+
+    python_blessed=(
+        "python.src"
+        "Enum"
+        "unittest"
+        "random"
+    )
+
+    error_message="The following imports are invalid (you may not use the standard library):"
+    gitroot=$(git rev-parse --show-toplevel)
+    cd $gitroot
+
+    java_invalid="$(git grep 'import.*java.util.*' java | grep -Ev $(join '|' ${java_blessed[@]}))"
+    if [[ $java_invalid != "" ]]; then
+        echo "$error_message"
+        echo "$java_invalid"
+        exit 1
+    fi
+
+    python_invalid="$(git grep 'import ' python | grep -Ev $(join '|' ${python_blessed[@]}))"
+    if [[ $python_invalid != "" ]]; then
+        echo "$error_message"
+        echo "$python_invalid"
+        exit 1
+    fi
+}
+
+main "$@"
